14f4267147edb7f0fa69403c7d4477d3f971c9d0
tim <jan-brenner@gmx.de>
2017-08-16 20:51:48 +0200

    Initial commit.

---
+++ CameraActivityChecker/Main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Management;
+
+namespace CameraActivityChecker
+{
+    public partial class Main : Form
+    {
+        private readonly NotifyIcon _trayIcon = new NotifyIcon();
+
+        public Main()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            InitializeTrayIcon();
+        }
+
+        private void Query()
+        {
+            var win32DeviceClassName = "win32_processor";
+            var query = $"select * from {win32DeviceClassName}";
+
+            using (var searcher = new ManagementObjectSearcher(query))
+            {
+                var objectCollection = searcher.Get();
+
+                foreach (var managementBaseObject in objectCollection)
+                {
+                    foreach (var propertyData in managementBaseObject.Properties)
+                    {
+                        Console.WriteLine(@"Property:  {0}, Value: {1}", propertyData.Name, propertyData.Value);
+                    }
+                }
+            }
+        }
+
+
+        private void InitializeTrayIcon()
+        {
+            _trayIcon.Text = @"";
+            _trayIcon.Visible = true;
+           // _trayIcon.Icon = Resources.ConnectedIcon;
+        }
+
+
+        protected override void SetVisibleCore(bool value)
+        {
+            base.SetVisibleCore(false);
+        }
+
+        private void SetIconSuccess()
+        {
+            //_trayIcon.Icon = Resources.ConnectedIcon;
+            //_trayIcon.BalloonTipTitle = Resources.InternetConnectionReestablished;
+            //_trayIcon.BalloonTipText = Resources.InternetConnectionReestablished;
+            //_trayIcon.Text = Resources.MainName + Environment.NewLine + Resources.Connected;
+            //_trayIcon.ShowBalloonTip(1000);
+        }
+
+        private void SetIconFail()
+        {
+            //_trayIcon.Icon = Resources.NotConnectedIcon;
+            //_trayIcon.BalloonTipTitle = Resources.InternetConnectionLost;
+            //_trayIcon.BalloonTipText = Resources.InternetConnectionLost;
+            //_trayIcon.Text = Resources.MainName + Environment.NewLine + Resources.NotConnected;
+            //_trayIcon.ShowBalloonTip(1000);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+CameraActivityChecker
+====================================
+
+CameraActivityChecker is a executable to check if the camera gets activated.
+The executable was written and tested in .Net 4.6.2.
+
+Change history
+--------------
+
+* **Version 1.0.0.0 (2017-08-16)** : 1.0 release.
