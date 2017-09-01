dccbee7324869559150ab9e675a23936cd327604
tim <jan-brenner@gmx.de>
2017-09-01 21:19:27 +0200

    Added check for camera activity.

--- WindowsFormsApp1/Main.cs
+++ WindowsFormsApp1/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Windows;
+using System.Threading;
+using System.Windows.Forms;
 using CameraActivityChecker.Notifications;
 using Emgu.CV;
 using Languages.Implementation;
@@ -7,13 +8,14 @@ using Languages.Interfaces;
 
 namespace CameraActivityChecker
 {
-    public partial class MainWindow : Window
+    public partial class Main : Form
     {
         private Capture _capture;
         private ILanguageManager _languageManager;
         private ILanguage _language;
+        private bool _cameraActivated;
 
-        public MainWindow()
+        public Main()
         {
             InitializeComponent();
             TryInitialize();
@@ -41,7 +43,6 @@ namespace CameraActivityChecker
 
         private void Initialize()
         {
-            Visibility = Visibility.Collapsed;
             _languageManager = new LanguageManager();
             _languageManager.SetCurrentLanguage("de-DE");
             _language = _languageManager.GetCurrentLanguage();
@@ -51,21 +52,37 @@ namespace CameraActivityChecker
         {
             try
             {
-                InitCamera();
-                var message = _language.GetWord("CameraActivated");
-                var toastNotification = new Notification(message, message, 1000, FormAnimator.AnimationMethod.Center, FormAnimator.AnimationDirection.Down);
-                toastNotification.Show();
+                var activated = IsCameraActivated();
+                if (_cameraActivated == activated) return;
+                _cameraActivated = activated;
+                if (!activated) return;
+                ShowNotification();
             }
             catch
             {
-                Console.WriteLine("Error");
+                // ReSharper disable once EmptyGeneralCatchClause
             }
         }
 
-        private void InitCamera()
+        private void ShowNotification()
+        {
+            var message = _language.GetWord("CameraActivated");
+            var toastNotification = GetNotification(message);
+            toastNotification.Show();
+            Thread.Sleep(2000);
+            toastNotification.Hide();
+        }
+
+        private Notification GetNotification(string message)
+        {
+            return new Notification(message, message, 1000, FormAnimator.AnimationMethod.Center, FormAnimator.AnimationDirection.Down);
+        }
+
+        private bool IsCameraActivated()
         {
             _capture = new Capture();
-            _capture.QueryFrame();
+            var frame =_capture.QueryFrame();
+            return frame != null;
         }
     }
 }
