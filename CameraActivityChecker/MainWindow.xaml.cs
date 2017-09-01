3b3f18ff58316e23b4b15b7607cadfac70c28872
tim <jan-brenner@gmx.de>
2017-09-01 20:09:05 +0200

    Added permanent checking of camera activation.

--- CameraActivityChecker/MainWindow.xaml.cs
+++ CameraActivityChecker/MainWindow.xaml.cs
@@ -1,15 +1,76 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using Emgu.CV;
+using ToastNotifications;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Messages;
+using ToastNotifications.Position;
 
 namespace CameraActivityChecker
 {
-    /// <summary>
-    ///     Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Capture _capture;
+        private Notifier _notifier;
+
         public MainWindow()
         {
             InitializeComponent();
+            TryInitialize();
+            CheckCameraActivated();
+        }
+
+        private void TryInitialize()
+        {
+            try
+            {
+                Initialize();
+            }
+            catch
+            {
+                Console.WriteLine("");
+            }
+        }
+
+        private void CheckCameraActivated()
+        {
+            while (true)
+                CheckCameraIsActive();
+        }
+
+        private void Initialize()
+        {
+            Visibility = Visibility.Collapsed;
+            InitToastMessages();
+        }
+
+        private void InitToastMessages()
+        {
+            _notifier = new Notifier(cfg =>
+            {
+                cfg.PositionProvider = new WindowPositionProvider(Application.Current.MainWindow, Corner.TopRight, 10, 10);
+                cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(TimeSpan.FromSeconds(3), MaximumNotificationCount.FromCount(5));
+                cfg.Dispatcher = Application.Current.Dispatcher;
+            });
+        }
+
+        private void CheckCameraIsActive()
+        {
+            try
+            {
+                InitCamera();
+                _notifier.ShowInformation("");
+            }
+            catch
+            {
+                Console.WriteLine("");
+            }
+        }
+
+        private void InitCamera()
+        {
+            _capture = new Capture();
+            _capture.QueryFrame();
         }
     }
 }
