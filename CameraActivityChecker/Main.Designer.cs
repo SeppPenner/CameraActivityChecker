3576be0ae6b2b34dcc84f30d96ce556a20e1e814
tim <jan-brenner@gmx.de>
2017-09-01 21:36:19 +0200

    Added icon.

--- CameraActivityChecker/Main.Designer.cs
+++ CameraActivityChecker/Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.SuspendLayout();
             // 
             // Main
@@ -35,9 +36,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.ShowInTaskbar = false;
-            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
